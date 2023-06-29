17dd4fba3a94b4ef654e25c60b4590225fae140a
Pankaj Manjhi <manjhi8pankaj@gmail.com>
2023-06-29 08:00:24 +0530

    adding files

---
+++ src/NewApp.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import { fetchWeather } from "./api/fetchWeather";
+import './App.css';
+
+
+const NewApp = () => {
+
+    const [query, setQuery] = useState('');
+    const [weather, setweather] = useState('');
+    const search = async(e) => {
+        if(e.key === 'Enter'){
+            const data = await fetchWeather(query);
+            setweather(data);
+            setQuery('');
+        }
+    }
+    return(
+        <div className="main-container">
+            <input 
+                name="text"
+                className="search"
+                placeholder="Search123..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+             />
+            
+            {
+                weather.main && (
+                    <div className="city">
+                        <h2 className="city-name">
+                            <span>{weather.name}</span>
+                            <sup>{weather.sys.country}</sup>
+                        </h2>
+                        <div className="city-temp">
+                            {Math.round(weather.main.temp)}
+                            <sup>&deg;C</sup>
+                        </div>
+                        <div className="info">
+                            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                            <p>{weather.weather[0].description}</p>
+                        </div>
+                    </div>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default NewApp;
